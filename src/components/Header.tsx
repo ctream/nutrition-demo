5de75f74d6c13d046c5bd677b1ba5cfba7337d16
davebd22 <salim.bozdag@ctream.de>
2023-08-28 12:53:32 +0200

    add cursor pointer to navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -54,7 +54,7 @@ const Menu = () => {
                   duration={1000}
                   key={item.name}
                   to={item.href}
-                  className="font-medium text-gray-500 hover:text-gray-900"
+                  className="font-medium text-gray-500 hover:text-gray-900 cursor-pointer"
                 >
                   {item.name}
                 </Link>
