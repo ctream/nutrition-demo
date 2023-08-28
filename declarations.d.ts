8e1f48ecbc0945dfba21b9a5d31501740570c385
davebd22 <salim.bozdag@ctream.de>
2023-08-28 12:17:55 +0200

    add rami-bot html tag and type declarations

---
+++ declarations.d.ts
@@ -0,0 +1,5 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    'rami-bot': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+  }
+}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,6 +14,10 @@ class MyDocument extends Document {
           src="https://dev.ram-i.ai/assets/index.0.0.30.js"
     ></script>
         <body>
+        <rami-bot
+      api-key="dd3e6e1e-b3e7-4a9d-9368-3d33a326a9b7"
+      is-overlay="true"
+    ></rami-bot>
           <Main />
           <NextScript />
         </body>
