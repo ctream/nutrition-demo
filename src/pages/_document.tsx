2eed7d17ce1e3581608ee9d24d3ba30d5140ed7b
davebd22 <salim.bozdag@ctream.de>
2023-08-30 10:02:53 +0200

    Update rami script version

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
         <script
           type="module"
           crossOrigin="true"
-          src="https://dev.ram-i.ai/assets/index.0.0.30.js"
+          src="https://dev.ram-i.ai/assets/index.0.0.33.js"
     ></script>
         <body>
         <rami-bot
